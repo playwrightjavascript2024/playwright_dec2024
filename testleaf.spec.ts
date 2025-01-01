040b84cf5a388850a760f4c72a1ef2924312001d
playwrightjavascript2024 <kubernetesonline@gmail.com>
2025-01-01 17:41:37 +0530

    Add files via upload

---
+++ testleaf.spec.ts
@@ -0,0 +1,25 @@
+import test, { chromium } from "@playwright/test";
+
+test("To launch a browser", async () => {
+
+    const browser = await chromium.launch();
+    
+    const browserContext = await browser.newContext();
+    
+    const page = await browserContext.newPage();
+    
+    await page.goto("https://login.salesforce.com/");
+
+    await page.fill('input[type="email"]', 'karthikeyan@testleaf.com');
+    await page.fill('input[type="password"]', 'UNceasing@92');
+
+    await page.click('input[name="Login"]');
+
+    await page.waitForTimeout(10000);
+    let title = await page.title()
+    console.log("The title is: " + title)
+
+    let url = await page.url()
+    console.log("The url is: " + url)
+
+   });
